3b51a4a119684d62380afe96c965d3d1f46acfd7
Nicho Lescano <neckolescano36@gmail.com>
2026-05-25 23:33:35 +0800

    asdasd

--- api/index.php
+++ api/index.php
@@ -1,17 +1,15 @@
 <?php
-// Set the root directory
-$root = __DIR__ . '/..';
 
-// Load composer autoloader
-require_once $root . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
-// Initialize Laravel application
-$app = require_once $root . '/bootstrap/app.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
-// Handle the request
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
 );
+
 $response->send();
+
 $kernel->terminate($request, $response);
