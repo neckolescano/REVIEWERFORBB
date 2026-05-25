bb018975187bf6ac1cd75a03709cb567cf0b947f
Nicho Lescano <neckolescano36@gmail.com>
2026-05-25 23:06:26 +0800

    poya

--- api/index.php
+++ api/index.php
@@ -1,7 +1,17 @@
 <?php
-// Force the current directory to be the project root
-chdir(__DIR__ . '/..');
+// Set the root directory
+$root = __DIR__ . '/..';
 
-// Include the autoloader and the public index
-require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../public/index.php';
+// Load composer autoloader
+require_once $root . '/vendor/autoload.php';
+
+// Initialize Laravel application
+$app = require_once $root . '/bootstrap/app.php';
+
+// Handle the request
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+$response->send();
+$kernel->terminate($request, $response);
