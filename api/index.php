68be434bc0b0706bee8808b88785ad1daa920175
Nicho Lescano <neckolescano36@gmail.com>
2026-05-25 23:03:02 +0800

    bago nsad

--- api/index.php
+++ api/index.php
@@ -1,3 +1,7 @@
 <?php
-// Forward Vercel requests to the public index file
-require __DIR__ . '/../public/index.php';
+// Force the current directory to be the project root
+chdir(__DIR__ . '/..');
+
+// Include the autoloader and the public index
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../public/index.php';
