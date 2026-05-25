74295db8c40a9921defbb2cca58c8e296cb7f5a0
Nicho Lescano <neckolescano36@gmail.com>
2026-05-25 22:41:23 +0800

    naa bago

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use App\Http\Controllers\QuizController;
 use Illuminate\Support\Facades\Route;
 
+// Standard routes for local development
 Route::get('/', [QuizController::class, 'index'])->name('quiz.index');
-Route::post('/submit', [QuizController::class, 'submit'])->name('quiz.submit'); 
+Route::post('/submit', [QuizController::class, 'submit'])->name('quiz.submit');
+
+// Vercel compatibility routes
+Route::get('/api', [QuizController::class, 'index']);
+Route::post('/api/submit', [QuizController::class, 'submit']);
